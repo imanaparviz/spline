8c44fdc6bb37081b302e6de6b968f92c46d14bf0
imanaparviz <imanaparviz@gmail.com>
2024-03-02 23:41:28 +0100

    lk

--- Script.js
+++ Script.js
@@ -247,4 +247,35 @@ page4.addEventListener("mouseenter",function(){
 }
 cursor4Effect()
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    
+    xDown = null;
+    yDown = null;                                             
+};
 
